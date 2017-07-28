61630db41e00308d48827821183ace4be8e6fb19
yellastro <yellastronaut@gmail.com>
2017-07-28 19:54:57 +0300

    немножко проработал процесс обновления прогресса

--- app/src/main/java/com/mycompany/mediatest2/RenameListAct.java
+++ app/src/main/java/com/mycompany/mediatest2/RenameListAct.java
@@ -11,22 +11,19 @@ public class RenameListAct extends  Activity
 {
 	private class RunRenaimingTask extends AsyncTask<Void, Integer, Integer> 
 	{ 
-		//ContentValues rootValue;
 		protected void onPreExecute()
 		{
-			//String roott=rootChose;
-			//rootValue = new ContentValues();
-			//rootValue.put("FAVORITE", rootChose);
-			collView.setText("starting...");
+			botButton.setText(R.string.stop_this);
+			botButton.setOnClickListener(onWTFPLZSTOPITNOW);
+			collView.setText(R.string.start_someth);
 		}
 		protected Integer doInBackground(Void... pp) 
 		{ 
-		
 			int i=0;
 			for(SongItem s: SongCorrector.items)
 			{i++;
 			try {
-				TimeUnit.SECONDS.sleep(1);
+				TimeUnit.MILLISECONDS.sleep(100);
 				
 			} catch (InterruptedException e) {
 				e.printStackTrace();
@@ -34,42 +31,60 @@ public class RenameListAct extends  Activity
 				 SongCorrector.renameSong(
 				 	new File(s.getAdress()),
 					SongCorrector.createNewName(s));
+				
 			}
-
 			return i;
 		}
 
-		
 		protected void onProgressUpdate(Integer... values)
 		{
 			super.onProgressUpdate(values);
 			collView.setText(Integer.toString(values[0]));
+			adapt.onChange(values[0]-1,1);
 		}
-		
-		
 		
 		protected void onPostExecute(Integer success) 
 		{
 			collView.setText(Integer.toString(success));
+			botButton.setText(R.string.start_list_rename);
+			botButton.setOnClickListener(onStartRename);
 		}
 	}
 	
+	private View.OnClickListener onWTFPLZSTOPITNOW = 
+	new View.OnClickListener()
+	{
+		@Override
+		public void onClick(View p1)
+		{Toast.makeText(getApplicationContext(), "one does not simply, just to take and stop some task..", Toast.LENGTH_SHORT) 
+			.show();}};
+
+	private View.OnClickListener onStartRename = 
+	new View.OnClickListener()
+	{
+		@Override
+		public void onClick(View p1)
+		{RunRenaimingTask tread=new RunRenaimingTask();
+			tread.execute();}};
+	
 	private TextView collView;
-	public static final String EXTRA_POSS="poss";
+	private Button botButton;
+	private RenameActAdapter adapt;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.rename_list_activuty);
-		//Intent intent=getIntent();
-		//intent.getIntArrayExtra(EXTRA_POSS);
+		
+		
 		collView=(TextView)findViewById(R.id.rename_list_coll_textview);
 		collView.setText("Rename "+Integer.toString(SongCorrector.items.size())
 			+" songs");
 		ListView list=(ListView)findViewById(R.id.rename_list_activutyListView);
-		
-		list.setAdapter(new RenameActAdapter(this,SongCorrector.items));
+		botButton=(Button)findViewById(R.id.rename_list_botButton);
+		adapt=new RenameActAdapter(this,SongCorrector.items);
+		list.setAdapter(adapt);
 		
 		
 	}
@@ -84,5 +99,4 @@ public class RenameListAct extends  Activity
 	{
 		finish();
 	}
-	
 }
